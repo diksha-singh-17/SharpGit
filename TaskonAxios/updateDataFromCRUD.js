c4ebaa000a036eafe528838fac366fa715f897a9
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-02-05 18:04:08 +0530

    updating data from CRUDCRUD

--- TaskonAxios/updateDataFromCRUD.js
+++ TaskonAxios/updateDataFromCRUD.js
@@ -14,57 +14,62 @@ function handleSubmitEvent(event) {
     tel: utel,
   };
 
-  const data = userDetails;
-  // JSON.stringify(userDetails);
-  console.log(data);
-  axios
-    .post(
-      "https://crudcrud.com/api/80a01e86934f4a209d5dd00c285bfb88/studentsRecordData",
+  if (obj.flag === true) {
+    axios.put(
+      `https://crudcrud.com/api/75400880c5744ae4b7cc88a0e4c7e881/studentsRecordData/${obj.id}`,
       {
-        data,
+        data: {
+          name: document.getElementById("uname").value,
+          email: document.getElementById("email").value,
+          tel: document.getElementById("tel").value,
+        },
       }
-    )
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      document.body.innerHTML = "Something went wrong, Check again:" + err;
-    });
+    );
+    obj.flag = false;
+  } else {
+    const data = userDetails;
+    axios
+      .post(
+        "https://crudcrud.com/api/75400880c5744ae4b7cc88a0e4c7e881/studentsRecordData",
+        { data }
+      )
+      .then((response) => {
+        console.info(response);
+      })
+      .catch((err) => {
+        console.error(err);
+        document.body.innerHTML = "Something went wrong, Check again:" + err;
+      });
+  }
   location.reload(true);
 }
 document.addEventListener("DOMContentLoaded", (event) => {
   event.preventDefault();
   axios
     .get(
-      "https://crudcrud.com/api/80a01e86934f4a209d5dd00c285bfb88/studentsRecordData"
+      "https://crudcrud.com/api/75400880c5744ae4b7cc88a0e4c7e881/studentsRecordData"
     )
     .then((response) => {
-      console.log("response from GET", response);
+      console.info("response from GET", response);
       for (let i = 0; i < response.data.length; i++) {
         showData(response.data[i]);
       }
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
       document.body.innerHTML = "Something went wrong, Check again:" + err;
     });
 });
 
 function showData(data) {
   const parsedData = data.data;
-  //  JSON.parse(data.data);
-  console.log(data, "****************data");
-  console.log(parsedData, parsedData.email);
   const list = document.getElementById("list");
   list.innerHTML += `<li id=${parsedData.email}>${parsedData.name}-${parsedData.email}-${parsedData.tel}
     <button id="delete-btn" onclick=deleteDetailsFromScreen('${parsedData.email}','${data._id}')>Delete</button>
-    <button id="edit-btn" onclick=EditDetailsFromScreen('${parsedData.email}','${parsedData.name}','${parsedData.tel}','${data._id}')>Edit</button>
+    <button id="edit-btn" onclick=EditDetailsFromScreen('${parsedData.name}','${parsedData.email}','${parsedData.tel}','${data._id}')>Edit</button>
     </li> `;
 }
 function deleteDetailsFromScreen(email, id) {
-  console.log(email, "email", id);
-  console.log(id);
   const parentNode = document.getElementById("list");
   const childNodeToBeDeleted = document.getElementById(email);
   if (childNodeToBeDeleted) {
@@ -73,35 +78,18 @@ function deleteDetailsFromScreen(email, id) {
 
   axios
     .delete(
-      `https://crudcrud.com/api/80a01e86934f4a209d5dd00c285bfb88/studentsRecordData/${id}`
+      `https://crudcrud.com/api/75400880c5744ae4b7cc88a0e4c7e881/studentsRecordData/${id}`
     )
     .catch((err) => {
-      console.log(err, err.message);
+      console.error(err, err.message);
       document.body.innerHTML = "Something went wrong, Check again:" + err;
     });
 }
 
-function EditDetailsFromScreen(email, name, tel, id) {
-  console.log("name-email-tel", id);
+function EditDetailsFromScreen(name, email, tel, id) {
+  obj.flag = true;
+  obj.id = id;
   document.getElementById("uname").value = name;
   document.getElementById("email").value = email;
   document.getElementById("tel").value = tel;
-
-  axios
-    .put(
-      `https://crudcrud.com/api/80a01e86934f4a209d5dd00c285bfb88/studentsRecordData/${id}`,
-
-      {
-        data: {
-          name: document.getElementById("uname").value,
-          email: document.getElementById("email").value,
-          tel: document.getElementById("tel").value,
-        },
-      }
-    )
-    .catch((err) => {
-      console.log(err);
-      document.body.innerHTML = "Something went wrong, Check again:" + err;
-    });
-  deleteDetailsFromScreen(email, id);
 }
